bc846d61be229ffe5df5a2771c82c3ff15bc93ff
GoktugYmsk <gogtug033@outlook.com>
2023-06-12 15:29:14 +0300

    sideBar deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Row, Col } from 'react-bootstrap';
 
 import Header from './components/header';
 import Content from './components/content';
-import SideBar from './components/sideBar';
 
 import './App.css';
 
@@ -19,11 +18,8 @@ function App() {
         </Col>
       </Row>
       <Row>
-        <Col sm={12} md={8} lg={8} >
+        <Col sm={12} md={12} lg={12} >
           <Content />
-        </Col>
-        <Col>
-          <SideBar md={4} lg={4} />
         </Col>
       </Row>
     </div>
